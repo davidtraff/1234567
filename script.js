fd4b167c8d066c7eb3cc82a440fa6e29ebaffd64
David Träff <traff.david@gmail.com>
2024-08-02 14:09:10 +0200

    skit

---
+++ script.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer');
+const config = require('./config.json');
+
+const sleep = (ms) => new Promise((resolve) => {
+    setTimeout(resolve, ms);
+});
+
+const MONTH_BUTTON = 'body > main > div > div > div._1maqrx41 > div.obah60.obah6j.obah63 > div:nth-child(3) > div.zn24yu0.zn24yu1 > button:nth-child(2)';
+const ORDER_BUTTON = 'body > main > div > div > div._1maqrx41 > div.obah60.obah6j.obah63 > div:nth-child(7) > button';
+
+const COMMERCE_TAG_SELECTOR = 'body > main > div > div > div > commerce-checkout';
+
+const addToCart = async (page) => {
+    await page.goto('https://www.comviq.se/mobiler/apple-iphone-15/3972/24/SPECIAL_OFFER-32989?accessories=');
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    await page.waitForSelector(MONTH_BUTTON);
+    await page.waitForSelector('#onetrust-accept-btn-handler');
+
+    page.click('#onetrust-accept-btn-handler');
+    await sleep(1000);
+
+    await page.click(MONTH_BUTTON);
+
+    await sleep(500);
+
+    await page.click(ORDER_BUTTON);
+};
+
+const evaluateElement = async (page, selector) => {
+    const path = `document.querySelector("body > main > div > div > div > commerce-checkout").shadowRoot.querySelector('${selector}')`;
+
+    return (await page.evaluateHandle(path)).asElement();
+};
+
+const getFormField = (page, name) => evaluateElement(page, `[name="${name}"]`);
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+
+    const page = await browser.newPage();
+
+    await addToCart(page);
+
+    await page.waitForSelector(COMMERCE_TAG_SELECTOR);
+    await sleep(1000);
+
+    await (await getFormField(page, 'ssn')).type(config.ssn);
+    await (await getFormField(page, 'email')).type(config.email);
+    await (await getFormField(page, 'phone')).type(config.phone);
+
+    await sleep(500);
+
+    const newNumberElement = await evaluateElement(page, '[aria-label="Nytt nummer"]');
+
+    await newNumberElement.click();
+
+    await sleep(2000);
+
+    while (true) {
+        const items = await page.evaluate(() => {
+            const numbers = document
+                .querySelector("body > main > div > div > div > commerce-checkout")
+                .shadowRoot
+                .querySelectorAll('div.cc_wpznfg17.cc_wpznfg18.cc_wpznfg1b');
+    
+            return Array.from(numbers).map((elem) => elem.textContent);
+        });
+    
+        console.log(items);
+        
+        await page.reload();
+        await page.waitForSelector(COMMERCE_TAG_SELECTOR);
+        await sleep(1000);
+    }
+})();
